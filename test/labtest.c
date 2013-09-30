088463ff3ef15533336bbba7fced9bf13e4de25e
Paul Smirnov <paulsmirnov@users.noreply.github.com>
2013-09-30 12:01:23 +0400

    Add a test project

---
+++ test/labtest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "../source/labengine.h"
+
+int main(void)
+{
+  // initialize
+  if (!LabInit())
+  {
+    printf("ERROR: Can not initialize the lab engine!\n");
+    return -1;
+  }
+
+  // do the job
+  printf("... here I do something...\nOK.\n");
+
+  // terminate
+  LabTerm();
+  return 0;
+}
